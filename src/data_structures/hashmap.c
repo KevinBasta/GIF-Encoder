700087f644d19aa778b4a3b911e2a0723a56588c
Kevin Basta <kevinbastadeveloper@gmail.com>
2023-08-18 00:46:32 -0400

    hashmap implementation complete

--- src/data_structures/hashmap.c
+++ src/data_structures/hashmap.c
@@ -22,13 +22,13 @@
 #endif //COMMON_UTIL
 
 
-size_t hashFunction(char *str) {
+size_t hashFunction(char *str, size_t size) {
     size_t i = 0;
 
     for (int j = 0; str[j]; j++)
         i += str[j];
     
-    return i % 10;
+    return i % size;
 }
 
 HashMapEntry *hashMapCreateEntry(char *key, char *value) {
@@ -53,7 +53,48 @@ HashMap *hashMapInit(size_t size) {
 }
 
 
+STATUS_CODE hashMapInsert(HashMap *map, char *key, char *value) {
+    size_t index = hashFunction(key, map->size);
+    HashMapEntry *newEntry = hashMapCreateEntry(key, value);
+    
+    // collision resolution: Open addressing
+    while (map->entries[index] != NULL && index < map->size) {
+        index++;
+    }
+    
+    HashMapEntry *currentIndexItem = map->entries[index];
+    if (currentIndexItem == NULL) {
+        if (map->currentCount == map->size) {
+            printf("hashmap insert fail\n");
+            freeHashMapEntry(newEntry);
+            return OPERATION_FAILED;
+        }
 
+        map->entries[index] = newEntry;
+        map->currentCount++; 
+    } else {
+        return OPERATION_FAILED;
+    }
+
+    return OPERATION_SUCCESS;
+}
+
+char *hashMapSearch(HashMap *map, char *key) {
+    int index = hashFunction(key, map->size);
+    HashMapEntry *entry = map->entries[index];
+
+    while (map->entries[index] != NULL &&
+           index < map->size &&
+           strncmp(entry->key, key, min(sizeof(entry->key), key) != 0)) {
+        index++;
+        entry = map->entries[index];
+    }
+
+    if (strncmp(entry->key, key, min(sizeof(entry->key), key) == 0))
+        return entry->value;
+
+    return NULL;
+}
 
 
 void printHashMap(HashMap *map) {
@@ -63,6 +104,17 @@ void printHashMap(HashMap *map) {
         }
     }
 }
+
+void printHashMapSearch(HashMap *map, char *key) {
+    char *value = hashMapSearch(map, key);
+
+    if (value == NULL) {
+        printf("Key [%s] not found\n", key);
+    } else {
+        printf("Key: [%s] \t Value: [%s]\n", key, value);
+    }
+}
+
 
 void freeHashMapEntry(HashMapEntry *entry) {
     free(entry->key);
--- src/data_structures/hashmap.h
+++ src/data_structures/hashmap.h
@@ -8,3 +8,14 @@ typedef struct HashMap {
     size_t currentCount;
     HashMapEntry **entries;
 } HashMap;
+
+HashMap *hashMapInit(size_t size);
+
+STATUS_CODE hashMapInsert(HashMap *map, char *key, char *value);
+char *hashMapSearch(HashMap *map, char *key);
+
+void printHashMap(HashMap *map);
+void printHashMapSearch(HashMap *map, char *key);
+
+void freeHashMapEntry(HashMapEntry *entry);
+void freeHashMap(HashMap *map);
